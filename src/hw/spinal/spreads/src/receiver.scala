c3083f72435fb1d943222c3ed7c79b65d7632973
Pedniel <d.pedder@hotmail.de>
2026-05-19 23:11:38 +0200

    Update receiver to use some kind of acquisition synchronization
    The lsfr just rolls on and on. The buffer gets the incoming chips.
    
    When we have a lsfr and buffer correlation, we correlate for our actual
    bit.

--- src/hw/spinal/spreads/src/receiver.scala
+++ src/hw/spinal/spreads/src/receiver.scala
@@ -1,26 +1,42 @@
 import spinal.core._
 
-case class Receiver(chips: Int, code: Seq[Int]) extends Component {
+case class Receiver(chips: Int, taps: Seq[Int]) extends Component {
   val io = new Bundle {
-    val rxChip     = in  Bool()
+    val enable     = in Bool()
+    val rxChip    = in  Bool()
     val rxValid    = in  Bool()
     val decodedBit = out Bool()
     val bitValid   = out Bool()
   }
 
-  val shiftReg = Vec.fill(chips)(RegInit(False))
+  
+  val lfsrReg = Vec.fill(chips)(RegInit(False))
+  lfsrReg(0).init(True)
+  val buffer  = Vec.fill(chips)(RegInit(False))
 
-  shiftReg(0) := RegNextWhen(io.rxChip, io.rxValid)
-  for (i <- 1 until chips)
-    shiftReg(i) := RegNextWhen(shiftReg(i - 1), io.rxValid)
+  // LFSr
+  var feedback = False
+  for (tap <- taps) { feedback = feedback ^ lfsrReg(tap - 1) }
+  when(io.enable) {
+    for (i <- chips - 1 downto 1) { lfsrReg(i) := lfsrReg(i - 1) }
+    lfsrReg(0) := feedback
+  }
 
-  val agreements = for (i <- code.indices) yield
-    if (code(i) == 1) shiftReg(i) else !shiftReg(i)
+  // buffer(chips-1) is NEWEST 
+  when(io.rxValid) {
+    for (i <- chips - 1 downto 1) { 
+      buffer(i) := buffer(i - 1) 
+    }
+    buffer(0) := io.rxChip
+  }
+
+  val agreements = for (i <- 0 until chips) yield {
+    buffer(i) === lfsrReg(i)
+  }
 
   var score = U(0, 8 bits)
   for (a <- agreements) score = score + a.asUInt(8 bits)
 
   io.decodedBit := score > U(chips / 2)
-  io.bitValid   := io.rxValid && (score > U(chips * 3 / 4) || score < U(chips / 4))
+  io.bitValid := io.rxValid && (score >= U(6) || score <= U(2))
 }
- 
