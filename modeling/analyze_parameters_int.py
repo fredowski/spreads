5a2b1f6baeb99147f9135d2fb94eb7d862a4f7a2
Thomas Homm <thomas-homm@t-online.de>
2026-05-20 09:58:39 +0200

    add notes, ideal vs real results

--- modeling/analyze_parameters_int.py
+++ modeling/analyze_parameters_int.py
@@ -10,14 +10,26 @@ from helper import bytesToPlusMinus, code_acquisition, decode, ber
 
 rng = np.random.default_rng()
 
-#binary noise:
-#Var(n) = N²
+# Variance PER SYMBOL
+# signal:
+# Var(s) = S² with amplitude S
+# binary noise:
+# Var(n) = N² with amplitude N
+# AWGN:  Var(n) = N_0/2 = N² with standard deviation N
+# Definition of SNR = E[s²]/E[n²] = S²/N²
 
-#AWGN: Variance PER SYMBOL N_0/2 = N²   
+# For binary noise/ interferer
+# Noise removal at constant N would be trivial, just measure average level and subtract if positive/add if negative
+# However in reality, there is a combination of AWGN and interfering signals, as well as changing signal levels
 
+# Todo
+# Compare ideal and real results to calculations using double instead of (clipped) int
+# Statistically compare bit error rate per SNR and code acquisition performance over large sample size
+# Visually they look identical
 
-#T = 1/(2W)
-#Energy per symbol S²/(2W) for signal, N²/(2W) for binary antipodal noise
+# Additional calculations:
+# T = 1/(2W) <=> 2W = 1/T
+# Energy per symbol S²/(2W) for signal, N²/(2W) for noise
 
 repeats = 1
 transmitted_bytes = 100
@@ -25,7 +37,7 @@ lfsr_size_max = 10
 snrs_db = np.array([0, -3, -6, -10, -13, -16, -20, -23, -26, -30, -33, -36, -40])
 
 S = math.floor(2**13 * (10**(0.05*(-40))))
-results = {"awgn" : {}, "bn" : {}}
+results = {"awgn" : {}, "bn" : {}, "awgn_ideal" : {}, "bn_ideal" : {}}
 for i in range(repeats):
     bits = (rng.integers(0,2,transmitted_bytes*8)*2-1)
     data =  (bits*S).astype(np.int64)
@@ -38,6 +50,8 @@ for i in range(repeats):
             N = S / (10**(0.05 * snr)) #voltage
             res_awgn = []
             res_bn = []
+            res_awgn_ideal = []
+            res_bn_ideal = []
             for offset in range(0,len(code),10):
                 padding = ((rng.integers(0,2,offset)*2-1)*S).astype(np.int64)
                 tx_code = code #np.roll(code,offset)
@@ -52,16 +66,26 @@ for i in range(repeats):
                 tx_sig_bn = tx_sig + binary_noise
                 offset_awgn = code_acquisition(tx_sig_awgn, code)
                 offset_bn = code_acquisition(tx_sig_bn, code)
-                data_awgn = decode(tx_sig_awgn, code, offset)[:len(data)]
-                data_bn = decode(tx_sig_bn, code, offset)[:len(data)]
+                #results with estimated offset
+                data_awgn = decode(tx_sig_awgn, code, offset_awgn)[:len(data)]
+                data_bn = decode(tx_sig_bn, code, offset_bn)[:len(data)]
                 ber_awgn = ber(bits, data_awgn)
                 ber_bn = ber(bits, data_bn)
                 res_awgn.append(ber_awgn)
                 res_bn.append(ber_bn)
+                #results if code acquisition always is successfull
+                data_awgn_ideal = decode(tx_sig_awgn, code, offset)[:len(data)]
+                data_bn_ideal = decode(tx_sig_bn, code, offset)[:len(data)]
+                ber_awgn_ideal = ber(bits, data_awgn)
+                ber_bn_ideal = ber(bits, data_bn)
+                res_awgn_ideal.append(ber_awgn)
+                res_bn_ideal.append(ber_bn)
                 print("AWGN SNR        : " + str(snr) + "; offset: " + str(offset) + "; detected offset: " + str(offset_awgn) + "; BER: " + str(ber_awgn))
                 print("Binary Noise SNR: " + str(snr) + "; offset: " + str(offset) + "; detected offset: " + str(offset_bn) + "; BER: " + str(ber_bn))
             results["awgn"][snr] = res_awgn
             results["bn"][snr] = res_bn
+            results["awgn_ideal"][snr] = res_awgn_ideal
+            results["bn_ideal"][snr] = res_bn_ideal
 
 fig, axs = plt.subplots(2)
 fig.suptitle("BER vs SNR")
