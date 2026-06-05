aeb3703c0cb7d917fc008f7e569dab9209f3d46c
Pedniel <d.pedder@hotmail.de>
2026-06-05 16:53:00 +0200

    Auto-format all files using Scala-Metals LSP
    Formatted using `Metals v2.0.0-M13 Gold` from [Metals LSP
    Github](https://github.com/scalameta/metals).
    
    Set up using Emacs with Eglot.

--- src/hw/spinal/spreads/src/receiver_analog.scala
+++ src/hw/spinal/spreads/src/receiver_analog.scala
@@ -1,36 +1,37 @@
 import spinal.core._
 import spinal.lib._
 
-case class Receiver_Analog(poly: Array[Int], m_lfsr: Int, n_adc: Int) extends Component {
+case class Receiver_Analog(poly: Array[Int], m_lfsr: Int, n_adc: Int)
+    extends Component {
   val io = new Bundle {
-    val enable = in Bool()
-    val signal  = in SInt(n_adc bits)
-    val data  = out Bool()
-    val syncd = out Bool()
+    val enable = in Bool ()
+    val signal = in SInt (n_adc bits)
+    val data = out Bool ()
+    val syncd = out Bool ()
   }
-  val chips = scala.math.pow(2,m_lfsr).toInt -1
+  val chips = scala.math.pow(2, m_lfsr).toInt - 1
 
   io.syncd := False
-  val lfsr = UnrollLFSR(poly.toArray, poly.max+1, 1, 1)
+  val lfsr = UnrollLFSR(poly.toArray, poly.max + 1, 1, 1)
   lfsr.io.skip := False
   lfsr.io.enable := io.enable
 
   // var data = False
   // val dataReg = RegNextWhen(data, io.syncd)
   io.data := False
-  //three parallel correlators
+  // three parallel correlators
   var acc = SInt((n_adc + m_lfsr) bits)
-  var accReg = RegNextWhen(acc, io.enable) init(0)
+  var accReg = RegNextWhen(acc, io.enable) init (0)
 
-  //TODO general method using scala fold or similar
-  when (lfsr.io.rnd_o(0) === False) {
+  // TODO general method using scala fold or similar
+  when(lfsr.io.rnd_o(0) === False) {
     acc := accReg +| io.signal
   } otherwise {
     acc := accReg -| io.signal
   }
 
-  var maxReg = Reg(UInt((n_adc + m_lfsr) bits)) init(0)
-  var offsetReg = Reg(UInt(m_lfsr bits)) init(0)
+  var maxReg = Reg(UInt((n_adc + m_lfsr) bits)) init (0)
+  var offsetReg = Reg(UInt(m_lfsr bits)) init (0)
 
   var accCount = Counter(1 to chips)
   var offsetCount = Counter((m_lfsr) bits)
@@ -43,15 +44,15 @@ case class Receiver_Analog(poly: Array[Int], m_lfsr: Int, n_adc: Int) extends Co
     val sSearch, sLocking, sLocked = newElement()
   }
 
-  val state = Reg(TrackState()) init(TrackState.sSearch)
+  val state = Reg(TrackState()) init (TrackState.sSearch)
   state.setName("ReceiverState")
   switch(state) {
     is(TrackState.sSearch) {
       // store result of iteration
-      when (lfsr.io.flag) {
+      when(lfsr.io.flag) {
         acc := 0
         offsetCount.increment()
-        when (accReg.abs > maxReg) {
+        when(accReg.abs > maxReg) {
           maxReg := accReg.abs
           offsetReg := accCount
         }
@@ -59,21 +60,21 @@ case class Receiver_Analog(poly: Array[Int], m_lfsr: Int, n_adc: Int) extends Co
       }
 
       // tried all offsets
-      when (offsetCount === offsetCount.maxValue) {
+      when(offsetCount === offsetCount.maxValue) {
         state := TrackState.sLocking
       }
 
     }
     is(TrackState.sLocking) {
       acc := 0
-      when ((accCount === (offsetReg)) && lfsr.io.flag) {
+      when((accCount === (offsetReg)) && lfsr.io.flag) {
         state := TrackState.sLocked
-      } elsewhen(lfsr.io.flag) {
+      } elsewhen (lfsr.io.flag) {
         lfsr.io.enable := False
       }
     }
     is(TrackState.sLocked) {
-      when (lfsr.io.flag) {
+      when(lfsr.io.flag) {
         io.syncd := True
         acc := 0
         when(accReg > 0) {
@@ -86,4 +87,3 @@ case class Receiver_Analog(poly: Array[Int], m_lfsr: Int, n_adc: Int) extends Co
     }
   }
 }
-
--- src/hw/spinal/spreads/src/top_analog.scala
+++ src/hw/spinal/spreads/src/top_analog.scala
@@ -3,27 +3,27 @@ import spinal.core._
 case class SpreadSpectrumTopAnalog(poly: List[Int]) extends Component {
   ClockDomain.current.clock.setName("CLOCK_50")
   val io = new Bundle {
-val txEnable = in Bool()
-val txData   = in Bool()
-val rxEnable = in Bool()
-val decoded  = out Bool()
-val syncd    = out Bool()
+    val txEnable = in Bool ()
+    val txData = in Bool ()
+    val rxEnable = in Bool ()
+    val decoded = out Bool ()
+    val syncd = out Bool ()
   }
 
-  val ngen = Channel(8,5)
+  val ngen = Channel(8, 5)
   val tx = Transmitter_Analog(poly)
   val rx = Receiver_Analog(poly.toArray, 10, 14)
 
-  ngen.io.enable:= io.txEnable
-  
-  tx.io.enable  := io.txEnable
-  tx.io.data    := io.txData
-  
-  rx.io.enable  := io.rxEnable
+  ngen.io.enable := io.txEnable
+
+  tx.io.enable := io.txEnable
+  tx.io.data := io.txData
+
+  rx.io.enable := io.rxEnable
 
   ngen.io.i := tx.io.coded
   rx.io.signal := ngen.io.o
-  
+
   io.decoded := rx.io.data
-  io.syncd   := rx.io.syncd
+  io.syncd := rx.io.syncd
 }
