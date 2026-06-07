b4d7c3462aa94f1bf2a7a4587efe93cc2c919f61
Pedniel <d.pedder@hotmail.de>
2026-06-07 02:57:03 +0200

    Set default AMOUNT_PARALLEL to 100 for testing
    This is just so that when other close this test branch they can test
    something easily without waiting for hours, which was the case with
    `AMOUNT_PARALLEL` set to `chips`.

--- src/hw/spinal/spreads/src/receiver_analog.scala
+++ src/hw/spinal/spreads/src/receiver_analog.scala
@@ -28,7 +28,7 @@ case class Receiver_Analog(poly: Array[Int], m_lfsr: Int, n_adc: Int)
 
   // TEST FOR MAX AMOUNT OF ACCS AND LFSRS
   // export JAVA_OPTS="-Xmx8g -Xss256m" needed
-  val AMOUNT_PARALLEL = chips  
+  val AMOUNT_PARALLEL = 100  
   val accs  = Vec(Reg(SInt((n_adc + m_lfsr) bits)) init(0), AMOUNT_PARALLEL)
   // We have to create different steps, else Quartus optimizes our code away because its all the same lfsr 
   var lfsrs = List[UnrollLFSR]()
