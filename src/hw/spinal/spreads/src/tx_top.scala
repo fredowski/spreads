8057276266cb19cbff9aa8a8916e010a871fccde
Pedniel <d.pedder@hotmail.de>
2026-06-12 00:31:44 +0200

    Update Makefile and top_analog to allow for top-level switching
    This allows us to switch between different top-level files.
    
    The code is not really pretty, but works for now.

--- src/hw/spinal/spreads/src/tx_top.scala
+++ src/hw/spinal/spreads/src/tx_top.scala
@@ -2,17 +2,27 @@ package spreads
 
 import spinal.core._
 
-case class SpreadSpectrumTopAnalog(poly: List[Int], symbols_to_integrate: Int, signal_attenuation_shifts: Int) extends Component {
+case class tx_top(poly: List[Int]) extends Component {
+  
+
   ClockDomain.current.clock.setName("CLOCK_50")
+  ClockDomain.current.reset.setName("reset")
+
   val io = new Bundle {
-    val txEnable = in Bool () setName("SW[0]")
-    val txData = in Bool () setName("SW[1]")
-    val coded = out SInt (14 bits)
-   }
+
+    val SW   = in Bits(10 bits) setName("SW")
+    val LEDR = out Bits(10 bits) setName("LEDR")
+    val LEDG = out Bits(8 bits) setName("LEDG")
+  }
+
 
   val tx = Transmitter_Analog(poly)
 
-  tx.io.enable := io.txEnable
-  tx.io.data := io.txData
-  io.coded := tx.io.coded
+  tx.io.enable := io.SW(0)
+  tx.io.data   := io.SW(1)
+
+  val codedBits = tx.io.coded.asBits
+  
+  io.LEDR := codedBits(9 downto 0)
+  io.LEDG := B"0000" ## codedBits(13 downto 10)
 }
