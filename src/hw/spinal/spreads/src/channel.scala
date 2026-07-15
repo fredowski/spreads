cecb720ac9937b55a3b099313e599174a87a251b
Pedniel <d.pedder@hotmail.de>
2026-07-15 10:41:44 +0200

    Update channel with new noise model

--- src/hw/spinal/spreads/src/channel.scala
+++ src/hw/spinal/spreads/src/channel.scala
@@ -21,28 +21,28 @@ case class Channel() extends Component {
   lfsr0.io.i_parallel := (default -> false)
   // x^31 + x^27 + x^23 + x^19 + x^15 + x^11 + x^10 + x^9 + x^7 + x^6 + x^5 + x^3 + x^2 + x^1 + 1
   val poly1 = List(30,26,22,18,14,10,9,8,6,5,4,2,1,0)
-  val lfsr1 = UnrollLFSR(poly1.toArray, poly1.max+1, 1, 12)
+  val lfsr1 = UnrollLFSR(poly1.toArray, poly1.max+1, 10, 12)
   lfsr1.io.skip := False
   lfsr1.io.enable := io.enable
   lfsr1.io.load := False
   lfsr1.io.i_parallel := (default -> false)
   // x^32 + x^22 + x^2 + x^1 + 1
   val poly2 = List(31,21,1,0)
-  val lfsr2 = UnrollLFSR(poly2.toArray, poly2.max+1, 2, 12)
+  val lfsr2 = UnrollLFSR(poly2.toArray, poly2.max+1, 8, 12)
   lfsr2.io.skip := False
   lfsr2.io.enable := io.enable
   lfsr2.io.load := False
   lfsr2.io.i_parallel := (default -> false)
   // x^32 + x^22 + x^21 + x^20 + x^18 + x^17 + x^15 + x^13 + x^12 + x^10 + x^8 + x^6 + x^4 + x^1 + 1
   val poly3 = List(31,21,20,19,17,16,14,12,11,9,7,5,3,0)
-  val lfsr3 = UnrollLFSR(poly3.toArray, poly3.max+1, 3, 12)
+  val lfsr3 = UnrollLFSR(poly3.toArray, poly3.max+1, 6, 12)
   lfsr3.io.skip := False
   lfsr3.io.enable := io.enable
   lfsr3.io.load := False
   lfsr3.io.i_parallel := (default -> false)
   // x^32 + x^28 + x^19 + x^18 + x^16 + x^14 + x^11 + x^10 + x^9 + x^6 + x^5 + x^1 + 1
   val poly4 = List(31,27,18,17,15,13,10,9,8,5,4,0)
-  val lfsr4 = UnrollLFSR(poly4.toArray, poly4.max+1, 1, 12)
+  val lfsr4 = UnrollLFSR(poly4.toArray, poly4.max+1, 7, 12)
   lfsr4.io.skip := False
   lfsr4.io.enable := io.enable
   lfsr4.io.load := False
